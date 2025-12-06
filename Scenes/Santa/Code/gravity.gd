c8a26814e90bcfefbecbed5a6a897b0208e4c14c
Pixelite22 <114366972+Pixelite22@users.noreply.github.com>
2025-12-06 13:23:30 -0500

    UPDATE 2
    Added State machine for santa, and the idle state

---
+++ Scenes/Santa/Code/gravity.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name gravity
+
+@export_subgroup("Settings")
+@export var gravityForce = 1000.0
+
+var is_falling = false
+
+func handleGravity(body : CharacterBody2D, delta): #Fundtion handles gravity
+	if not body.is_on_floor(): #If the character body isn't on the floor
+		body.velocity.y += gravityForce * delta #add the gravity to the character until they land
+	
+	is_falling = body.velocity.y > 0 and not body.is_on_floor() #sets isFalling flag if body ix falling and not onn ground
